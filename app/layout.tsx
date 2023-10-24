57105f2eb7126004640adf67fe1e5ba3f37a0eb0
santhosh <august.vs.sant@gmail.com>
2023-10-24 08:15:15 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'United Heaven',
+  description: 'United Heaven is an organization seeking to create a finer world through blockchain technology.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
